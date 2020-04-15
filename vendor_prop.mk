10d64dc627ffa94ebd861e2880283c03ee3c689b
Ankan005 <ankan005@gmail.com>
2020-04-15 11:50:00 +0000

    Set lmkd configuration properties

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -200,3 +200,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0
+
+#lmkd
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=0 \
+    ro.lmk.critical=0 \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100
