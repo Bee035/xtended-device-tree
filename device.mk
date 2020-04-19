edc4a05d9201e96a802d89629b53b1ce1af8ff0b
ankan005 <ankan005@gmail.com>
2020-04-19 08:38:50 +0000

    update CNE/DPM blobs from daisy - QKQ1.191002.002 V11.0.2.0.QD

--- device.mk
+++ device.mk
@@ -336,7 +336,7 @@ PRODUCT_PACKAGES += \
     libxml2
 
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.1 \
+    android.hardware.radio@1.4 \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0
 
